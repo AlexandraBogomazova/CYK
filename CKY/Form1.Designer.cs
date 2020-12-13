26a52f7fd72b93d0f98775767de9265dbfe1b29d
Bogomazova Alexandra <alexandra.bogomazova888@gmail.com>
2020-12-13 20:18:29 +0300

    Добавьте файлы проекта.

---
+++ CKY/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace CKY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Grammar = new System.Windows.Forms.DataGridView();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridViewResult = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelAns = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Grammar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Thistle;
+            this.label1.Font = new System.Drawing.Font("Ravie", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(416, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(584, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Алгоритм Кока-Янгера-Касами";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ravie", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1416, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Grammar
+            // 
+            this.Grammar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grammar.Location = new System.Drawing.Point(113, 112);
+            this.Grammar.Name = "Grammar";
+            this.Grammar.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Grammar.Size = new System.Drawing.Size(165, 245);
+            this.Grammar.TabIndex = 2;
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(242, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Грамматика в НФХ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(468, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(221, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Входящая строка";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(492, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 29);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(390, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(373, 115);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(407, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 30);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "w = ";
+            // 
+            // dataGridViewResult
+            // 
+            this.dataGridViewResult.AllowUserToAddRows = false;
+            this.dataGridViewResult.AllowUserToDeleteRows = false;
+            this.dataGridViewResult.AllowUserToOrderColumns = true;
+            this.dataGridViewResult.AllowUserToResizeColumns = false;
+            this.dataGridViewResult.AllowUserToResizeRows = false;
+            this.dataGridViewResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResult.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dataGridViewResult.Location = new System.Drawing.Point(821, 112);
+            this.dataGridViewResult.Name = "dataGridViewResult";
+            this.dataGridViewResult.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewResult.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridViewResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridViewResult.Size = new System.Drawing.Size(559, 496);
+            this.dataGridViewResult.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(924, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 30);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Результат";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Ravie", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(323, 438);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 43);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Ответ";
+            // 
+            // labelAns
+            // 
+            this.labelAns.AutoSize = true;
+            this.labelAns.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAns.Location = new System.Drawing.Point(108, 516);
+            this.labelAns.Name = "labelAns";
+            this.labelAns.Size = new System.Drawing.Size(605, 30);
+            this.labelAns.TabIndex = 12;
+            this.labelAns.Text = "*                                                                      * ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(1465, 650);
+            this.Controls.Add(this.labelAns);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridViewResult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Grammar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Grammar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelAns;
+        public System.Windows.Forms.DataGridView Grammar;
+        public System.Windows.Forms.DataGridView dataGridViewResult;
+    }
+}
+
---
+++ CKY/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CKY
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            //abb
+            Grammar.Columns.Add("1", "1");
+            Grammar.Columns.Add("2", "-");
+            Grammar.Columns.Add("3", "2");
+            Grammar.Columns[0].Width = 25;
+            Grammar.Columns[1].Width = 25;
+            Grammar.Columns[2].Width = 25;
+            Grammar.Rows.Add("S", "->", "AB");
+            Grammar.Rows.Add("A", "->", "CD");
+            Grammar.Rows.Add("A", "->", "CF");
+            //Grammar.Rows.Add("A", "->", "AD");
+            Grammar.Rows.Add("B", "->", "c");
+            Grammar.Rows.Add("B", "->", "EB");
+            Grammar.Rows.Add("C", "->", "a");
+            Grammar.Rows.Add("D", "->", "b");
+            Grammar.Rows.Add("E", "->", "c");
+            Grammar.Rows.Add("F", "->", "AD");
+            //Grammar.Rows.Add("F", "->", "DD");
+            //Grammar.Rows.Add("K", "->", "CF");
+        }
+        public int kol = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kol++;
+            if (kol>1)
+            {
+                Form1_Load(sender, e);
+            }
+            if (textBox1.Text != "")
+            {
+                string input = textBox1.Text;
+                string[][] matrix = new string[input.Length][];
+                int k = 0;
+                for (int i = input.Length; i > 0; --i)
+                {
+                    matrix[k] = new string[i];
+                    for (int j = 0; j < matrix[k].Length; ++j)
+                    {
+                        matrix[k][j] = "";
+                    }
+                    k++;
+                }
+                for (int i = 0; i < input.Length; ++i)
+                {
+                    dataGridViewResult.Columns.Add($"{i + 1}", input[i].ToString());
+                    dataGridViewResult.Columns[i].Width = 50;
+                    dataGridViewResult.Rows.Add();
+                }
+                MainAlgorythm(ref matrix, input, this);
+                k = 0;
+                for (int i = 0; i < matrix.Length; ++i)
+                {
+                    for (int j = 0; j < matrix[k].Length; ++j)
+                    {
+                        dataGridViewResult[j, i].Value = matrix[k][j];
+                    }
+                    k++;
+                }
+                for (int i = 0; i < matrix.Length; ++i)
+                {
+                    for (int j = 0; j < matrix.Length; ++j)
+                    {
+                        if (dataGridViewResult[j, i].Value == null)
+                        {
+                            dataGridViewResult[j, i].Style.BackColor=Color.Transparent;
+                        }
+
+                    }
+                }
+                Regex r = new Regex("S");
+                if (r.IsMatch(dataGridViewResult[0, input.Length-1].Value.ToString()))
+                {
+                    labelAns.Text = "В заданной контекстно-свободной грамматике \n можно вывести заданную строку";
+                }
+                else
+                {
+                    labelAns.Text = "В заданной контекстно-свободной грамматике \n нельзя вывести заданную строку";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите входящую строку");
+            }
+        }
+
+        public static void MainAlgorythm(ref string[][] matrix, string input, Form1 f)
+        {
+            int k = 0;
+            int x1, y1, x2, y2;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                x1 = 0;
+                x2 = i - 1;
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    y1 = j;
+                    y2 = j + 1;
+                    if (x2 <= -1)
+                    {
+                        x1 = 0;
+                        x2 = i - 1;
+                    }
+                    if (i == 0)
+                    {
+                        matrix[k][j] = FirstLine(input[j].ToString(), f);
+                    }
+                    else
+                    {
+                        while (x1 != i)
+                        {
+                            Change(ref matrix[k][j], matrix[x1][y1], matrix[x2][y2], f);
+                            x1++;
+                            x2--;
+                            y2++;
+                        }
+                    }
+                }
+                k++;
+            }
+        }
+
+        public static string FirstLine(string First, Form1 f)
+        {
+            string ans = "";
+            string g;
+            string[] Terminals = First.Split(',');
+            for (int i = 0; i < Terminals.Length; ++i)
+            {
+                for (int j = 0; j < f.Grammar.Rows.Count - 1; ++j)
+                {
+                    g = f.Grammar[2, j].Value.ToString();
+                    if (g == Terminals[i])
+                    {
+                        if (ans.Length > 0)
+                        {
+                            ans += ",";
+                        }
+                        ans += f.Grammar[0, j].Value.ToString();
+                    }
+                }
+            }
+            return ans;
+        }
+
+        public static void Change(ref string before, string First, string Sec, Form1 f)
+        {
+            string symb;
+            string[] Terminals1 = First.Split(',');
+            string[] Terminals2 = Sec.Split(',');
+            foreach (var t1 in Terminals1)
+            {
+                foreach (var t2 in Terminals2)
+                {
+                    symb = t1 + t2;
+                    for (int j = 0; j < f.Grammar.Rows.Count - 1; ++j)
+                    { 
+                        if (symb == f.Grammar[2, j].Value.ToString())
+                        {
+                            if (before.Length > 0)
+                            {
+                                before += ",";
+                            }
+                            before += f.Grammar[0, j].Value.ToString();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
